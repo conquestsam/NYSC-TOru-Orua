4bd951da90a056837d3b5cd74f298749cef3055d
Sammy-D-Coder <qambbz@gmail.com>
2025-07-08 12:28:16 -0700

    apos

--- project/components/CDSProjectsSection.tsx
+++ project/components/CDSProjectsSection.tsx
@@ -91,7 +91,7 @@ export default function CDSProjectsSection() {
               Past CDS Projects
             </h2>
             <p className="text-xl text-gray-600 max-w-2xl mx-auto">
-              Discover the impactful Community Development Service projects we&apos;ve completed
+              Discover the impactful Community Development Service projects we'&apos;'ve completed
             </p>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
